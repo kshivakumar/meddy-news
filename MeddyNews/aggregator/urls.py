2173067b1edd3abe8af75235d68dcc6febe4da2c
Shiva Kumar <kshivakumar@outlook.com>
2019-09-15 19:50:14 +0530

    rest_framework setup, add placeholder view

---
+++ MeddyNews/aggregator/urls.py
@@ -0,0 +1,10 @@
+"""Aggregator URLconfs"""
+
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.get_news)
+]
--- MeddyNews/aggregator/views.py
+++ MeddyNews/aggregator/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+"""Aggregator Views"""
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def get_news(request):
+    query_params = request.query_params
+    if query_params:
+        return Response('News Search')
+    # latest news
+    return Response('Latest News')
