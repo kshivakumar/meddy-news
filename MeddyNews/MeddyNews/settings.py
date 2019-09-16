ffabef4b880bcf46ad64cabbfa7402daa72dd7f5
Shiva Kumar <kshivakumar@outlook.com>
2019-09-16 09:20:54 +0530

    Add DRF authentication settings

--- MeddyNews/MeddyNews/settings.py
+++ MeddyNews/MeddyNews/settings.py
@@ -69,6 +69,16 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 WSGI_APPLICATION = 'MeddyNews.wsgi.application'
 
 
